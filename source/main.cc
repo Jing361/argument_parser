b7262bf6123333869376b5a2355e414012cac3d8
Chris Samuelson <chris.sam55@gmail.com>
2016-07-28 15:44:33 -0400

    Added multi-parameter arguments test

--- source/main.cc
+++ source/main.cc
@@ -18,18 +18,48 @@ public:
   wrapper(string str):
     mStr(str){
   }
-  const string& get(){
+  const string& get() const{
     return mStr;
   }
 };
 
-istream& operator>>(istream& is, wrapper& wrap){
+istream& operator>>(istream& is, wrapper& w){
   string str;
 
   is >> str;
 
-  wrapper w(str);
-  wrap = w;
+  w = wrapper(str);
+
+  return is;
+}
+
+class doublewide{
+private:
+  string val1;
+  string val2;
+
+public:
+  doublewide() = default;
+  doublewide(string str1, string str2):
+    val1(str1),
+    val2(str2){
+  }
+  const string& get1() const{
+    return val1;
+  }
+  const string& get2() const{
+    return val2;
+  }
+};
+
+istream& operator>>(istream& is, doublewide& w){
+  string str1;
+  string str2;
+
+  is >> str1;
+  is >> str2;
+
+  w = doublewide(str1, str2);
 
   return is;
 }
@@ -39,12 +69,14 @@ TEST_CASE("Arguments can be checked by existence", "[parser]"){
   const char* argw[] = { "main", 
                          "--test", "a",
                          "--aoeu", "1",
+                         "--htns", "abc", "xyz",
                          "--str", "test",
                          "-test", "b" };
-  int argd = 9;
+  int argd = 12;
 
   ap.add_argument("--test", 1);
   ap.add_argument("--aoeu", 1);
+  ap.add_argument("--htns", 2);
   ap.add_argument("--str", 1);
   ap.add_argument("-test");
   ap.add_argument("none");
@@ -62,6 +94,10 @@ TEST_CASE("Arguments can be checked by existence", "[parser]"){
 
     REQUIRE(ap.get_argument<string>("--test") == string("a"));
     REQUIRE(ap.get_argument<wrapper>("--str").get() == string("test"));
+
+    REQUIRE(ap.get_argument<doublewide>("--htns").get1() == string("abc"));
+    REQUIRE(ap.get_argument<doublewide>("--htns").get2() == string("xyz"));
+
     REQUIRE(ap.get_argument<int>("--aoeu") == 1);
     REQUIRE(ap.get_argument<bool>("-test"));
   }
