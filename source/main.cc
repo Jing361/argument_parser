bb7fed5f6bd0a8f57eb938e4e8217d34b23f357c
Chris Samuelson <chris.sam55@gmail.com>
2016-07-28 15:13:01 -0400

    Replaced lazy testing with some unit testing

--- source/main.cc
+++ source/main.cc
@@ -9,31 +9,6 @@
 
 using namespace std;
 
-/*int main(int argc, char** argv){
-  argparse ap;
-
-  ap.add_argument("--test", 1);
-  ap.add_argument("-test");
-  ap.add_argument("none");
-  ap.parse_args(argc, argv);
-  cout << boolalpha;
-  cout << ap.get_argument<string>("--test") << endl;
-  cout << ap.get_argument<bool>("-test") << endl;
-  cout << ap.get_argument<bool>("none") << endl;
-  try{
-    cout << ap.get_argument<bool>("fail") << endl;
-  } catch(std::exception& e){
-
-  }
-  try{
-    cout << ap.get_argument<string>("fail") << endl;
-  } catch(std::exception& e){
-
-  }
-
-  return 0;
-}*/
-
 TEST_CASE("Arguments can be checked by existence", "[parser]"){
   argparse ap;
   const char* argw[] = { "main", 
@@ -44,10 +19,20 @@ TEST_CASE("Arguments can be checked by existence", "[parser]"){
   ap.add_argument("--test", 1);
   ap.add_argument("-test");
   ap.add_argument("none");
-  try{
-  ap.parse_args(argd, argw);
-  } catch(argumentNotFoundException& e){
-    REQUIRE(e.culprit() == string("b"));
+
+  SECTION("Catches unspecified arguments"){
+    try{
+      ap.parse_args(argd, argw);
+    } catch(argumentNotFoundException& e){
+      REQUIRE(e.culprit() == string("b"));
+    }
+  }
+
+  SECTION("Arguments can be checked after parsing"){
+    ap.parse_args(--argd, argw);
+
+    REQUIRE(ap.get_argument<string>("--test") == string("a"));
+    REQUIRE(ap.get_argument<bool>("-test"));
   }
 }
 
